0396fb31d08c7a45276fe62478a69e817dd9dff2
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-11 15:18:20 +0300

    Created DbContext-class. Made CodeFirst

---
+++ HW14/HotelBookingLibrary/Data/HotelBookingDbContext.cs
@@ -0,0 +1,59 @@
+﻿using HotelBookingLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingLibrary.Data
+{
+	public class HotelBookingDbContext : DbContext
+	{
+		public virtual DbSet<Hotel> Hotels { get; set; }
+		public virtual DbSet<HotelRoom> HotelRooms { get; set; }
+		public virtual DbSet<RoomReservation> RoomsReservations { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BloggingPlatform;Integrated Security=True");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Hotel>()
+				.Property(h => h.Name)
+				.IsRequired();
+
+			modelBuilder.Entity<Hotel>()
+				.Property(h => h.StarRating)
+				.IsRequired();
+
+			modelBuilder.Entity<HotelRoom>()
+				.Property(hr => hr.Name)
+				.IsRequired();
+
+			modelBuilder.Entity<HotelRoom>()
+				.Property(hr => hr.Price)
+				.IsRequired();
+
+			modelBuilder.Entity<HotelRoom>()
+				.Property(hr => hr.RoomType)
+				.IsRequired();
+
+			modelBuilder.Entity<RoomReservation>()
+				.Property(rr => rr.StartDateTime)
+				.IsRequired();
+
+			modelBuilder.Entity<RoomReservation>()
+				.Property(rr => rr.EndDateTime)
+				.IsRequired();
+
+
+			modelBuilder.Entity<Hotel>()
+				.HasMany<HotelRoom>(h => h.HotelRooms)
+				.WithOne(hr => hr.Hotel)
+				.HasForeignKey(hr => hr.HotelId);
+
+			modelBuilder.Entity<HotelRoom>()
+				.HasMany<RoomReservation>(hr => hr.RoomReservations)
+				.WithOne(rr => rr.HotelRoom)
+				.HasForeignKey(rr => rr.HotelRoomId);
+		}
+	}
+}
---
+++ HW14/HotelBookingLibrary/Migrations/20241011121305_InitialCommit.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StarRating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    RoomType = table.Column<int>(type: "int", nullable: false),
+                    HotelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelRooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HotelRooms_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomsReservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HotelRoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomsReservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoomsReservations_HotelRooms_HotelRoomId",
+                        column: x => x.HotelRoomId,
+                        principalTable: "HotelRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelRooms_HotelId",
+                table: "HotelRooms",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomsReservations_HotelRoomId",
+                table: "RoomsReservations",
+                column: "HotelRoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomsReservations");
+
+            migrationBuilder.DropTable(
+                name: "HotelRooms");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
