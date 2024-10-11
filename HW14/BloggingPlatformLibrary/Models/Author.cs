bca085d7cbe2543db8707bf82e96539f63eea55d
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-11 12:51:47 +0300

    Created models

---
+++ HW14/BloggingPlatformLibrary/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace BloggingPlatformLibrary.Models
+{
+	public class Author
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public string Description { get; set; } = null!;
+
+		public ICollection<Article>? Articles { get; set; }
+	}
+}
