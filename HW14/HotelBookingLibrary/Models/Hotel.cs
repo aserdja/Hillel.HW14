24a7f65f6bd6232275fc61e0716334e24ab25eb3
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-11 15:05:02 +0300

    Created models

---
+++ HW14/HotelBookingLibrary/Models/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingLibrary.Models
+{
+	public class Hotel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public StarRating StarRating { get; set; }
+
+		public ICollection<HotelRoom>? HotelRooms { get; set; }
+	}
+}
---
+++ HW14/HotelBookingLibrary/Models/RoomReservation.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelBookingLibrary.Models
+{
+	public class RoomReservation
+	{
+		public int Id { get; set; }
+		public DateTime StartDateTime { get; set; }
+		public DateTime EndDateTime { get; set; }
+
+		public int HotelRoomId { get; set; }
+		public HotelRoom HotelRoom { get; set; } = null!;
+	}
+}
