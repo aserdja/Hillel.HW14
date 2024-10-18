f15d2d1162cd82ea1cae718ea54f70e1c7731352
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-18 21:23:58 +0300

    Update Program.cs

--- HW14/HW14/Program.cs
+++ HW14/HW14/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibraryPlatform.Data;
+using LibraryPlatform.Models;
+using LibraryPlatform.Repositories;
+using LibraryPlatform.Services;
+using System.Runtime.InteropServices;
+
+using var context = new LibraryPlatformDbContext();
+var br = new BookRepository(context);
+var brr = new BookRentalRepository(context);
+var ur = new UserRepository(context);
+
+var bs = new BookService(br);
+var brs = new BookRentalService(br, brr);
+
+var user1 = ur.GetAll().Result.First();
+var user2 = ur.GetAll().Result.Last();
+
+var book1 = br.GetByTitle("1984").Result.First();
+var book2 = br.GetByTitle("Atlant").Result.First();
+
+Console.WriteLine(await brs.CheckBookAvailability(book1, DateTime.Now.Date.AddDays(1), DateTime.Now.Date.AddDays(3))); 
+await brs.RentBook(user1, book1, DateTime.Now.Date.AddDays(1), DateTime.Now.Date.AddDays(3));
+Console.WriteLine(await brs.CheckBookAvailability(book1, DateTime.Now.Date.AddDays(1), DateTime.Now.Date.AddDays(3))); 
+await brs.RentBook(user2, book1, DateTime.Now.Date.AddDays(1), DateTime.Now.Date.AddDays(3));
+await brs.RentBook(user2, book2, DateTime.Now.Date.AddDays(3), DateTime.Now.Date.AddDays(5));
+await brs.RentBook(user2, book1, DateTime.Now.Date.AddDays(6), DateTime.Now.Date.AddDays(10));
+
